5f44d17b97bdf0579ddabefff3f14dedf9c274e5
atharvbhavsar <atharvbhavsar99@gmail.com>
2025-01-21 21:46:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+document.querySelector('.div2').addEventListener('click', function() {
+    const membersDiv = document.querySelector('.members');
+    if (membersDiv) {
+      membersDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else {
+      console.error('No element with the class "members" found.');
+    }
+  });
+  document.querySelector('.div1').addEventListener('click', function() {
+    const membersDiv = document.querySelector('.banner-container');
+    if (membersDiv) {
+      membersDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else {
+      console.error('No element with the class "members" found.');
+    }
+  });
+  document.querySelector('.div3').addEventListener('click', function() {
+    const membersDiv = document.querySelector('.join-us');
+    if (membersDiv) {
+      membersDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else {
+      console.error('No element with the class "members" found.');
+    }
+  });
+  document.querySelector('.div4').addEventListener('click', function() {
+    const membersDiv = document.querySelector('.event-page');
+    if (membersDiv) {
+      membersDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else {
+      console.error('No element with the class "members" found.');
+    }
+  });
+  document.querySelector('.div5').addEventListener('click', function() {
+    const membersDiv = document.querySelector('.about-us');
+    if (membersDiv) {
+      membersDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else {
+      console.error('No element with the class "members" found.');
+    }
+  });
+  document.querySelector('.div6').addEventListener('click', function() {
+    const membersDiv = document.querySelector('.gallery');
+    if (membersDiv) {
+      membersDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else {
+      console.error('No element with the class "members" found.');
+    }
+  });
+  document.querySelector('.div7').addEventListener('click', function() {
+    const membersDiv = document.querySelector('.contact-us');
+    if (membersDiv) {
+      membersDiv.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    } else {
+      console.error('No element with the class "members" found.');
+    }
+  });
+  
+
+
+  
